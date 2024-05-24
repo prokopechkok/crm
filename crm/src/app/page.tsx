ec3b28d863ce1574aec989cec83e30ea15d62e26
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2024-05-24 15:27:16 +0300

    client and server components, base routing

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,13 +1,17 @@
+import { headers } from 'next/headers';
 import StatusLabel, { Status } from './components/status-label';
+import AddCompanyButton from './components/add-company-button';
 
 export default function Home() {
+  console.log(headers());
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <AddCompanyButton />
+      {/* <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel> */}
     </main>
   );
 }
